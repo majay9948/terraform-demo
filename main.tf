e2226fde0339b17aef59721f7986676b0c125670
majay9948 <majay9948@gmail.com>
2022-08-16 17:48:52 +0530

    ec2

--- main.tf
+++ main.tf
@@ -81,6 +81,12 @@ resource "aws_key_pair" "ssh-key" {
   key_name   = "server-key"
   public_key = file(var.public_key_location)
 }
+output "aws_ami_id" {
+  value = data.aws_ami.amazon-ami-image.id
+}
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
 resource "aws_instance" "myapp-server" {
   ami                         = data.aws_ami.amazon-ami-image.id
   instance_type               = var.instance_type
@@ -89,14 +95,8 @@ resource "aws_instance" "myapp-server" {
   availability_zone           = var.avail_zone
   associate_public_ip_address = true
   key_name                    = aws_key_pair.ssh-key.key_name
-
+  user_data                   = file("entry-script.sh")
   tags = {
     Name = "${var.env_prefix}-server"
   }
-}
-output "aws-ami-ids" {
-  value = data.aws_ami.amazon-ami-image.id
-}
-output "ec2-public-ip" {
-  value = aws_instance.myapp-server.public_ip
 }
